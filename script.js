8a35c4ab7255190376e8bf1ea9e6679c08d22203
mychonc1 <mychon_c@yahoo.com>
2024-12-12 16:29:02 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+let code1 = 5 + 5;
+let code2 = 20 * 2;
+let code3 = 40 - 1;
+alert(message + code1 + code2 + code3);
