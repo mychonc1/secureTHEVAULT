46ad45a369302c38a3bd4c057e5601ceb8643fd9
mychonc1 <mychon_c@yahoo.com>
2024-12-12 19:40:04 -0500

    added comments

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+//create index.html file
+//create external script.js file
+//insert script tags in the internal file
+//insert your external file inside your script tags.
+//go to your external file
+//let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 let message =
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//let code1= 10
 let code1 = 5 + 5;
+//let code2= 40
 let code2 = 20 * 2;
+//let code3= 39
 let code3 = 40 - 1;
+//combined message and all 3 codes
 alert(message + code1 + code2 + code3);
